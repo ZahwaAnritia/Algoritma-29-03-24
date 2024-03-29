c0dcbe9b6357bb5ea49cec32a5c9ee5ab605a9c0
ZahwaAnritia <anritia15@gmail.com>
2024-03-29 11:07:39 +0700

    mencari elemen yang lebih besar

--- Algoritma-29-03-24/Algoritma-29-03-24.cpp
+++ Algoritma-29-03-24/Algoritma-29-03-24.cpp
@@ -57,3 +57,17 @@ void q_short(int low, int high)
 	j = high; //step 4
 
 	while (i <= j) //step 10
+	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high)) //step 5
+		{
+			i++; //step 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element Less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) //step 7
+		{
+			j--; //step 8
+			cmp_count++;
+		}
